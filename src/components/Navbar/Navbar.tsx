366788c4ad078c8076516e988bf669639660f060
Farid <faridkarmanov@gmail.com>
2022-08-02 15:15:01 +0300

    feat(pr): finished project

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,30 @@
 import "./navbar.scss";
-import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 export const Navbar = () => {
   return (
-    <div className="container">
-      <nav className="nav">
-        <a href="!#" className="nav__link">
-          <Logo />
-        </a>
-        <ul className="nav__list">
-          <li className="nav__item">
-            <a href="!#" className="nav__item-link">
-              Features
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="!#" className="nav__item-link">
-              Partners
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="!#" className="nav__item-link">
-              Stories
-            </a>
-          </li>
-        </ul>
-        <button className="nav__button">Download for free</button>
-      </nav>
-    </div>
+    <nav className="nav">
+      <a href="!#" className="nav__link">
+        <Logo />
+      </a>
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="!#" className="nav__item-link">
+            Features
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="!#" className="nav__item-link">
+            Partners
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="!#" className="nav__item-link">
+            Stories
+          </a>
+        </li>
+      </ul>
+      <button className="nav__button">Download for free</button>
+    </nav>
   );
 };
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,14 +2,23 @@ import "./slider.scss";
 
 export const Slider = () => {
   return (
-    <div className="slider-container">
-      <div className="content">
-        <h2 className="content__title">The Power of Simplicity</h2>
-        <h2 className="content__description">
-          Instead of spending time searching for the right app, our AI will
-          bring the right app to you.
-        </h2>
-        <button className="content__button">Learn</button>
+    <div className="slider">
+      <div className="slider__container">
+        <div className="slider__slide">
+          <h2 className="slider__title">The Power of Simplicity</h2>
+          <p className="slider__description">
+            Instead of spending time searching for the right app, our AI will
+            bring the right app to you.
+          </p>
+          <button className="slider__button">Learn</button>
+          <div className="slider__dots">
+            <div className="slider__dot"></div>
+            <div className="slider__dot"></div>
+            <div className="slider__dot--active"></div>
+            <div className="slider__dot"></div>
+            <div className="slider__dot"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
